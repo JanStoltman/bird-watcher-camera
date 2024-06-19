28195085ff9934709b2acafd65b2ab11220c3d34
janstoltman <stoltmanjan@gmail.com>
2024-06-19 10:13:10 +0300

    Update

--- main.py
+++ main.py
@@ -1,40 +1,67 @@
-from picamera2 import Picamera2, Preview
-import time
-import cv2
+#!/usr/bin/python3
+
+import os
 import shutil
+import subprocess
+import time
 
-picam2 = Picamera2()
-capture_config = picam2.create_still_configuration()
-preview_config = picam2.create_preview_configuration(main={"size": (640, 480)})
+import numpy as np
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder, Quality
+from picamera2.outputs import FileOutput
 
-picam2.configure(preview_config)
-picam2.start()
 
-bird_cascade = cv2.CascadeClassifier("bird_model.xml")
-face_cascade = cv2.CascadeClassifier("face_model.xml")
-counter = 0
+def convert_to_mp4(h_filename):
+    print(f"Converting to mp4 {h_filename}")
+    mp_filename = h_filename.replace(".h264", ".mp4")
+    subprocess.run(f"MP4Box -add {h_filename} {mp_filename}")
+    os.remove(h_filename)
 
-while True:
-    time.sleep(2)
-    print(".")
-    imageArray = picam2.capture_array("main")
 
-    birds = bird_cascade.detectMultiScale(imageArray, 1.1, 3)
-    faces = face_cascade.detectMultiScale(imageArray,1.1, 3)
+def pre_start_cleanup():
+    print("Cleaning up previous recordings")
+    if os.path.exists("rec/"):
+        shutil.rmtree("rec/")
+    os.makedirs("rec/")
 
-    if hasattr(birds, "size") and birds.size > 0:
-        print("--------------------------- Bird detected!")
-        imgName = "bird" + str(counter) + ".jpg"
-        picam2.switch_mode_and_capture_file(capture_config, imgName)
-        shutil.move(imgName, "pics/" + imgName)
-        counter += 1 
 
-    if hasattr(faces, "size") and faces.size > 0:
-        print("Face detected!")
-        # imgName = "face" + str(counter) + ".jpg"
-        # picam2.switch_mode_and_capture_file(capture_config, imgName)
-        # shutil.move(imgName, "pics/" + imgName)
-        # counter += 1
+if __name__ == '__main__':
+    lsize = (320, 240)
+    picam2 = Picamera2()
+    video_config = picam2.create_video_configuration(
+        main={"size": (1280, 720), "format": "RGB888"},
+        lores={"size": lsize, "format": "YUV420"}
+    )
 
-    if counter >= 100:
-        break
+    picam2.configure(video_config)
+    encoder = H264Encoder()
+    picam2.start()
+
+    w, h = lsize
+    prev = None
+    encoding = False
+    ltime = 0
+    filename = ""
+
+    while True:
+        cur = picam2.capture_buffer("lores")
+        cur = cur[:w * h].reshape(h, w)
+        if prev is not None:
+            # Measure pixels differences between current and
+            # previous frame
+            mse = np.square(np.subtract(cur, prev)).mean()
+            if mse > 6 or encoding and mse > 3:
+                if not encoding:
+                    filename = f"rec/{int(time.time())}.h264"
+                    encoder.output = FileOutput(filename)
+                    picam2.start_encoder(encoder, quality=Quality.VERY_HIGH)
+                    encoding = True
+                    print("New Motion", mse)
+                ltime = time.time()
+            else:
+                if encoding and time.time() - ltime > 3.0:
+                    picam2.stop_encoder()
+                    encoding = False
+                    print("Stopped encoding")
+                    convert_to_mp4(filename)
+        prev = cur
